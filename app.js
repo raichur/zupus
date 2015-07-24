1cb392746ee4236f2860e5da4049148d5cbca3b0
Joshua Raichur <hello@joshuaraichur.com>
2015-07-25 00:16:08 +0530

    Add: Randomizer

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+var items = [];
+
 $.getJSON("planetarynames.json", function(data){
-  var items = [];
   $.each( data, function( key, val ) {
     items.push( "<li id='" + val.Feature_ID + "'>" + val.Feature_Name + "</li>" );
   });
   $('body').append("<ul>" + items.join("") + "</ul>");
+});
+
+$('#random').click(function(e) {
+  e.preventDefault();
+  $('#output').html(items[Math.floor(Math.random()*items.length)]);
 });
