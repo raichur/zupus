2b40e69a25339b5eca4b6d18f862fd49d4ebe120
Joshua Raichur <hello@joshuaraichur.com>
2015-07-25 09:54:27 +0530

    Fix: Minor tweaks

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $.getJSON("planetarynames.json", function(data){
   $.each( data, function( key, val ) {
     items.push( "<li id='" + val.Feature_ID + "'>" + val.Feature_Name + "</li>" );
   });
-  $('body').append("<ul>" + items.join("") + "</ul>");
+  $('body').append("<ul id='names'>" + items.join("") + "</ul>");
 });
 
 $('#random').click(function(e) {
@@ -15,3 +15,16 @@ $('#random').click(function(e) {
 $(document).ajaxComplete(function() {
   $('#output').html(items[Math.floor(Math.random()*items.length)]);
 });
+
+function filter(element) {
+    var value = $(element).val().toLowerCase();
+
+    $("#names > li").each(function() {
+        if ($(this).text().toLowerCase().search(value) > -1) {
+            $(this).show();
+        }
+        else {
+            $(this).hide();
+        }
+    });
+}
